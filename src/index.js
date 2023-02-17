9f089e6da36017eaf0cf011fcca4f11fc4007085
Amaka Konwea <lawrahcollections@gmail.com>
2023-02-17 07:55:07 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import './index.css';
-import { Provider } from 'react-redux';
-import store from './Redux/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
