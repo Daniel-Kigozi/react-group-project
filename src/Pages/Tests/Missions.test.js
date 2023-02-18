6980d0c23f12466a3faf801c958a10633175a9b8
Daniel-Kigozi <kdanalex69@gmail.com>
2023-02-18 18:55:36 +0300

    Fix linter errors

--- src/Pages/Tests/Missions.test.js
+++ src/Pages/Tests/Missions.test.js
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import Missions from '../Missions';
 import store from '../../Redux/configureStore';
 
-describe("Missions page", () => {
-it('renders correctly', () => {
-  const Mission = renderer.create(
+describe('Missions page', () => {
+  it('renders correctly', () => {
+    const Mission = renderer.create(
       <Provider store={store}><Missions /></Provider>,
     )
-    .toJSON();
-  expect(Mission).toMatchSnapshot();
+      .toJSON();
+    expect(Mission).toMatchSnapshot();
+  });
 });
-})
