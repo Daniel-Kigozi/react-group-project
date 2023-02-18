77e4aaa7b8cfa09a1ccb2c8360dde3919cb8c71f
Daniel-Kigozi <kdanalex69@gmail.com>
2023-02-18 16:44:25 +0300

    Add css to profile and mission

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,18 +1,24 @@
 import { useSelector } from 'react-redux';
+import MissionProfile from './missionProfile';
 
 const Profile = () => {
   const { rocketlist } = useSelector((state) => state.rockets);
   const reservedRocket = rocketlist.filter((rocket) => rocket.reserved === true);
   return (
     <div className="rocketListDiv">
-      <h1 className="myRocketsHeader">My Rockets</h1>
-      <ul className="rocketListContainer">
-        {reservedRocket.map((list) => (
-          <li className="rocketList" key={list.id}>
-            {list.rocketName}
-          </li>
-        ))}
-      </ul>
+      <span className="profile-view">
+        <MissionProfile />
+      </span>
+      <span className="profile-view">
+        <h1 className="myRocketsHeader">My Rockets</h1>
+        <ul className="rocketListContainer">
+          {reservedRocket.map((list) => (
+            <li className="rocketList" key={list.id}>
+              {list.rocketName}
+            </li>
+          ))}
+        </ul>
+      </span>
     </div>
   );
 };
