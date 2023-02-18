65241312c0822958972e7db9a2e19d3bbafbc582
Amaka Konwea <lawrahcollections@gmail.com>
2023-02-18 10:58:02 +0000

    Bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './Redux/configureStore';
+import store from '../Redux/configureStore';
 import './index.css';
 import App from './App';
 
